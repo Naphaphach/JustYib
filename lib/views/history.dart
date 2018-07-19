bcb13a7f1401c9fbe56be45a5f35a58fd2b407ab
Kamontat Chantrachirathumrong <kamontat.c@hotmail.com>
2018-07-19 17:30:17 +0700

    [release] Build code 3

---
+++ lib/views/history.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:kcapstone/components/cartList.dart';
+import 'package:kcapstone/models/cart.dart';
+
+class History extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("รายการของฉัน"),
+      ),
+      body: ListView(
+        children: SingletonCart().onlyTakenCart().map((cart) {
+          return CartList(cart);
+          // return Text(cart.totalPrice().toString());
+        }).toList(),
+      ),
+    );
+  }
+}
--- lib/views/order.dart
+++ lib/views/order.dart
@@ -3,17 +3,11 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:kcapstone/components/cartList.dart';
 import 'package:kcapstone/models/cart.dart';
+import 'package:kcapstone/views/history.dart';
 
 import 'package:kcapstone/views/summary.dart';
 
 class Order extends StatelessWidget {
-  // final List<String> randomStatus = ["กำลังปรุง", "ปรุงสำเร็จ", "รอคอนเฟิร์ม"];
-
-//  String random() {
-//    randomStatus.shuffle(Random());
-//    return randomStatus.first;
-//  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,6 +32,8 @@ class Order extends StatelessWidget {
                 icon: Icon(Icons.history),
                 onPressed: () {
                   print("History");
+                  Navigator.push(
+                      context, MaterialPageRoute(builder: (_) => History()));
                 })
           ],
         ),
